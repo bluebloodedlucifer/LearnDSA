55d4cf1397ca73bc828a3f035adc6d39798c07a4
bluebloodedlucifer <aryanpanchal.1999@gmail.com>
2022-02-08 17:39:48 +0530

    corrected RotateANumber.java

--- Level_1/1_Basics_of_Programming/Getting_Started/RotateANumber.java
+++ Level_1/1_Basics_of_Programming/Getting_Started/RotateANumber.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.sound.sampled.SourceDataLine;
-
 public class RotateANumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
