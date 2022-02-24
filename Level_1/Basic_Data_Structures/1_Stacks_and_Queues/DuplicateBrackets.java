bcaf9e6c8ce6077ca32a2bab4af152c0dd44efcd
bluebloodedlucifer <aryanpanchal.1999@gmail.com>
2022-02-24 23:59:00 +0530

    Started Stacks and Queues

---
+++ Level_1/Basic_Data_Structures/1_Stacks_and_Queues/DuplicateBrackets.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * DuplicateBrackets
+ */
+public class DuplicateBrackets {
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == ')'){
+                if(st.peek() == '('){
+                    System.out.println("true");
+                    input.close();
+                    return;
+                }else{
+                    while(st.peek() != '('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }else{
+                st.push(ch);
+            }
+        }
+        System.out.println("false");
+        input.close();
+    }
+}
